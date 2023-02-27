6323eb24280f3663de55e74b41a166d95424c8fa
Ovidijus Parsiunas <oparsiunas@googlemail.com>
2023-02-27 22:07:09 +0000

    initial speech to text implementation code

--- component/src/views/chat/input/input.ts
+++ component/src/views/chat/input/input.ts
@@ -1,22 +1,23 @@
 import {KeyboardInput} from './keyboardInput/keyboardInput';
 import {SubmitButton} from './submitButton/submitButton';
+import {SpeechInput} from './speechInput/speechInput';
 import {Messages} from '../messages/messages';
 
 export class Input {
   readonly elementRef: HTMLElement;
-  private readonly _button: SubmitButton;
+  private readonly _submitButton: SubmitButton;
 
   constructor(key: string, messages: Messages) {
     this.elementRef = document.createElement('div');
     this.elementRef.id = 'input';
-    const inputElement = new KeyboardInput();
-    this._button = new SubmitButton(inputElement.inputElementRef, key, messages);
-    inputElement.submit = this._button.submit.bind(this._button);
-    this.addElements(inputElement.elementRef, this._button.elementRef);
+    const keyboardInput = new KeyboardInput();
+    this._submitButton = new SubmitButton(keyboardInput.inputElementRef, key, messages);
+    keyboardInput.submit = this._submitButton.submit.bind(this._submitButton);
+    const speechInput = new SpeechInput(keyboardInput.inputElementRef);
+    this.addElements(keyboardInput.elementRef, this._submitButton.elementRef, speechInput.elementRef);
   }
 
-  private addElements(inputElement: HTMLElement, buttonElement: HTMLElement) {
-    this.elementRef.appendChild(inputElement);
-    this.elementRef.appendChild(buttonElement);
+  private addElements(...elements: HTMLElement[]) {
+    elements.forEach((element) => this.elementRef.appendChild(element));
   }
 }
