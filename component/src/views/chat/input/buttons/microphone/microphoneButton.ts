b469504759ae3b768623f6bc7c859ae31729a927
Ovidijus Parsiunas <oparsiunas@googlemail.com>
2023-03-08 21:40:44 +0000

    disabled microphone

--- component/src/views/chat/input/buttons/microphone/microphoneButton.ts
+++ component/src/views/chat/input/buttons/microphone/microphoneButton.ts
@@ -10,6 +10,7 @@ import {CustomStyle} from '../../../../../types/styles';
 export class Microphone {
   readonly elementRef: HTMLElement;
   private readonly _microphoneIcon: SVGGraphicsElement;
+  private readonly _recognition?: SpeechRecognition;
   private readonly _activeStyle: CustomStyle = {
     filter: `brightness(0) saturate(100%) invert(72%) sepia(5%) saturate(4533%) hue-rotate(183deg) brightness(100%) contrast(102%)`,
   };
@@ -18,6 +19,12 @@ export class Microphone {
   constructor() {
     this._microphoneIcon = Microphone.createSVGIconElement();
     this.elementRef = this.createMicrophoneElement();
+    const speechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    if (!speechRecognition) {
+      this.elementRef.classList.add('disabled-microphone');
+    } else {
+      this._recognition = new speechRecognition();
+    }
     // this._inputElementRef = inputElement;
   }
 
@@ -47,10 +54,9 @@ export class Microphone {
   }
 
   private start() {
-    const speechRecognition = webkitSpeechRecognition;
-    const recognition = new speechRecognition();
-    recognition.start();
-    recognition.onresult = function (event) {
+    if (!this._recognition) return;
+    this._recognition.start();
+    this._recognition.onresult = function (event) {
       console.log(event);
       // const speechResult = event.results[0][0].transcript.toLowerCase();
       // for (var i = event.resultIndex; i < event.results.length; ++i) {
@@ -63,47 +69,47 @@ export class Microphone {
 
       // console.log(`Confidence: ${event.results[0][0].confidence}`);
     };
-    // recognition.onspeechend = function () {
-    //   recognition.stop();
+    // this._recognition.onspeechend = function () {
+    //   this._recognition.stop();
     //   // testBtn.disabled = false;
     //   // testBtn.textContent = 'Start new test';
     // };
 
-    // recognition.onerror = function (event) {
+    // this._recognition.onerror = function (event) {
     //   // testBtn.disabled = false;
     //   // testBtn.textContent = 'Start new test';
     //   // diagnosticPara.textContent = `Error occurred in recognition: ${event.error}`;
     // };
 
-    recognition.onaudiostart = Microphone.recordingStarted.bind(this, this._microphoneIcon, this._activeStyle);
+    this._recognition.onaudiostart = Microphone.recordingStarted.bind(this, this._microphoneIcon, this._activeStyle);
 
-    recognition.onaudioend = Microphone.recordingEnded.bind(this, this._microphoneIcon, this._activeStyle);
+    this._recognition.onaudioend = Microphone.recordingEnded.bind(this, this._microphoneIcon, this._activeStyle);
 
-    // recognition.onend = function (event) {
+    // this._recognition.onend = function (event) {
     //   //Fired when the speech recognition service has disconnected.
     //   console.log('SpeechRecognition.onend');
     // };
 
-    // recognition.onnomatch = function (event) {
-    //   //Fired when the speech recognition service returns a final result with no significant recognition. This may involve some degree of recognition, which doesn't meet or exceed the confidence threshold.
+    // this._recognition.onnomatch = function (event) {
+    //   //Fired when the speech recognition service returns a final result with no significant this._recognition. This may involve some degree of recognition, which doesn't meet or exceed the confidence threshold.
     //   console.log('SpeechRecognition.onnomatch');
     // };
 
-    // recognition.onsoundstart = function (event) {
+    // this._recognition.onsoundstart = function (event) {
     //   //Fired when any sound — recognisable speech or not — has been detected.
     //   console.log('SpeechRecognition.onsoundstart');
     // };
 
-    // recognition.onsoundend = function (event) {
+    // this._recognition.onsoundend = function (event) {
     //   //Fired when any sound — recognisable speech or not — has stopped being detected.
     //   console.log('SpeechRecognition.onsoundend');
     // };
 
-    // recognition.onspeechstart = function (event) {
+    // this._recognition.onspeechstart = function (event) {
     //   //Fired when sound that is recognised by the speech recognition service as speech has been detected.
     //   console.log('SpeechRecognition.onspeechstart');
     // };
-    // recognition.onstart = function (event) {
+    // this._recognition.onstart = function (event) {
     //   //Fired when the speech recognition service has begun listening to incoming audio with intent to recognize grammars associated with the current SpeechRecognition.
     //   console.log('SpeechRecognition.onstart');
     // };
