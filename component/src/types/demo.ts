53329e2b9c47c2faa4d9b42032c82667f242283f
Ovidijus Parsiunas <oparsiunas@googlemail.com>
2023-06-18 23:41:02 +0100

    removing custom and documentation for the demo

--- component/src/types/demo.ts
+++ component/src/types/demo.ts
@@ -3,5 +3,5 @@ import {ErrorMessageOverrides} from './messages';
 export type DemoErrors = {[key in keyof ErrorMessageOverrides]?: boolean};
 
 export type Demo =
-  | boolean
+  | true
   | {displayErrors?: DemoErrors; displayLoadingBubble?: boolean; displayFileAttachmentContainer?: boolean};
