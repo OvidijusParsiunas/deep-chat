7a4be72cd691e539663a0080f4e7ce66e615510a
Ovidijus Parsiunas <oparsiunas@googlemail.com>
2023-10-11 17:38:32 +0100

    bug fix for stateful styles as the styles were applied to the children elements

--- component/src/utils/element/statefulEvents.ts
+++ component/src/utils/element/statefulEvents.ts
@@ -2,34 +2,29 @@ import {StatefulStyles} from '../../types/styles';
 import {StyleUtils} from './styleUtils';
 
 export class StatefulEvents {
-  private static mouseUp(styles: StatefulStyles, event: MouseEvent) {
-    const element = event.target as HTMLElement;
+  private static mouseUp(element: HTMLElement, styles: StatefulStyles) {
     StyleUtils.unsetAllCSSMouseStates(element, styles);
     Object.assign(element.style, styles.default);
     Object.assign(element.style, styles.hover);
   }
 
-  private static mouseDown(styles: StatefulStyles, event: MouseEvent) {
-    const element = event.target as HTMLElement;
+  private static mouseDown(element: HTMLElement, styles: StatefulStyles) {
     Object.assign(element.style, styles.click);
   }
 
-  private static mouseLeave(styles: StatefulStyles, event: MouseEvent) {
-    const element = event.target as HTMLElement;
+  private static mouseLeave(element: HTMLElement, styles: StatefulStyles) {
     StyleUtils.unsetAllCSSMouseStates(element, styles);
     Object.assign(element.style, styles.default);
   }
 
-  private static mouseEnter(styles: StatefulStyles, event: MouseEvent) {
-    const element = event.target as HTMLElement;
+  private static mouseEnter(element: HTMLElement, styles: StatefulStyles) {
     Object.assign(element.style, styles.hover);
   }
 
-  // note that children elements can not be selectable
   public static add(element: HTMLElement, styles: StatefulStyles) {
-    element.addEventListener('mouseenter', StatefulEvents.mouseEnter.bind(this, styles));
-    element.addEventListener('mouseleave', StatefulEvents.mouseLeave.bind(this, styles));
-    element.addEventListener('mousedown', StatefulEvents.mouseDown.bind(this, styles));
-    element.addEventListener('mouseup', StatefulEvents.mouseUp.bind(this, styles));
+    element.addEventListener('mouseenter', StatefulEvents.mouseEnter.bind(this, element, styles));
+    element.addEventListener('mouseleave', StatefulEvents.mouseLeave.bind(this, element, styles));
+    element.addEventListener('mousedown', StatefulEvents.mouseDown.bind(this, element, styles));
+    element.addEventListener('mouseup', StatefulEvents.mouseUp.bind(this, element, styles));
   }
 }
