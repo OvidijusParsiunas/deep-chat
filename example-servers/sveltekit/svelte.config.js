d61bd55513bb152b0afe35d4aba8f0d42474fdc0
Ovidijus Parsiunas <oparsiunas@googlemail.com>
2024-03-13 21:21:06 +0000

    updating sveltekit package dependencies

--- example-servers/sveltekit/svelte.config.js
+++ example-servers/sveltekit/svelte.config.js
@@ -1,12 +1,18 @@
-import {vitePreprocess} from '@sveltejs/kit/vite';
+import {vitePreprocess} from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-auto';
 // VERCEL - switch to the following adapter instead when deploying to Vercel
 // import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
   preprocess: vitePreprocess(),
+
   kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
   },
 };
