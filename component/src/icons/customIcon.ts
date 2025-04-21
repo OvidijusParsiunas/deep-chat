b30d57238eb717c7b194d9cfacc5252ded0f3cb7
Ovidijus Parsiunas <oparsiunas@googlemail.com>
2025-04-21 23:55:03 +0900

    reducing the line width of custom icon svgs

--- component/src/icons/customIcon.ts
+++ component/src/icons/customIcon.ts
@@ -2,6 +2,6 @@
 // prettier-ignore
 export const CUSTOM_ICON_STRING =
 `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22" fill="none">
-  <rect x="2.5" y="2.5" width="17" height="17" rx="2" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  <rect x="2.5" y="2.5" width="17" height="17" rx="2" stroke="#000000" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
 `;
